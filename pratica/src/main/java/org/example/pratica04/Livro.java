055409cfeff44bb643785109917f79d14d77b348
RafaelBorgesCalil <rafaelborgescalil@discente.ufg.br>
2023-04-24 21:42:23 -0300

    update pratica04

--- pratica/src/main/java/org/example/pratica04/Livro.java
+++ pratica/src/main/java/org/example/pratica04/Livro.java
@@ -1,2 +1,51 @@
-package org.example.pratica04;public class Livro {
+package org.example.pratica04;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private Integer ano;
+    private String editora;
+
+    public String toString(String string){
+        return string;
+    }
+
+    public Livro(String titulo, String autor, Integer ano,String editora){
+        setAno(ano);
+        setAutor(autor);
+        setEditora(editora);
+        setTitulo(titulo);
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
 }
--- pratica/src/main/java/org/example/pratica04/TestaLivro.java
+++ pratica/src/main/java/org/example/pratica04/TestaLivro.java
@@ -1,2 +1,7 @@
-package org.example.pratica04;public class TestaLivro {
+package org.example.pratica04;
+
+public class TestaLivro {
+    public static void main(String[] args) {
+
+    }
 }
